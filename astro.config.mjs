10a80dcd966377a86760d33cfa461edec75bd320
Gurkan <hello@gurkz.me>
2026-03-19 15:53:43 +0100

    setup deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: "https://thatgurkangurk.github.io",
+  base: "/juggernaut-docs",
+  output: "static",
+  integrations: [
+    starlight({
+      title: "Juggernaut Docs",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/thatgurkangurk/juggernaut-docs",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
